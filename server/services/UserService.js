419cd4f94091652d01bec57a83f999d7421fb21b
Justin <jingyu120@gmail.com>
2022-02-12 16:29:51 -0600

    fix bug where login with invalid username fetches null user data

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -4,11 +4,9 @@ const UserModel = require("../models/User");
 export const createUser = async (data) => {
   const user = await UserModel.findOne({ username: data.username });
   if (user) {
-    console.log("exists");
     return "exist";
   } else {
     await new UserModel(data).save();
-    console.log("success");
     return "success";
   }
 };
@@ -16,7 +14,7 @@ export const createUser = async (data) => {
 export const verifyByUsername = async (data) => {
   const { username, password } = data;
   const user = await UserModel.findOne({ username });
-  if (password === user.password) {
+  if (user && password === user.password) {
     return user.email;
   } else {
     return null;
